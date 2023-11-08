cf1be0ebfc0e098fe3d146ce7c59239a519aa37f
pa1rane <ranepavanrp@gmail.com>
2023-11-08 19:00:38 +0530

    integrated redux toolkit

--- src/Redux/membersSlice.js
+++ src/Redux/membersSlice.js
@@ -5,3 +5,17 @@ const initialState = {
     memberList : members,
 }
 
+
+const membersSlice = createSlice({
+    name:"members",
+    initialState, 
+    reducers : {
+        addMember : (state, action) => {
+           state.memberList += action.payload
+        }
+    }
+})
+
+export const {addMember} = membersSlice.actions;
+
+export default membersSlice.reducer;
